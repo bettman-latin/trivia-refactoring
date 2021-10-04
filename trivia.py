810edcdf27c01c835ac5b5e7f470090315bf5f35
bettman_latin <lbettman@lsoc.org>
2021-10-04 11:34:47 -0500

    working player class | working version

--- trivia.py
+++ trivia.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 #test
+#correct V
+#self.players[currenty_player].whatever
+
+
+class Player:
+    def __init__(self, n):
+        self.name = n
+        self.places = 0
+        self.purses = 0
+        self.in_penalty_box = 0
+
+
 class Game:
     def __init__(self):
-        self.players = []
-        self.places = [0] * 6
-        self.purses = [0] * 6
-        self.in_penalty_box = [0] * 6
+
+        self.players = []        
 
         self.pop_questions = []
         self.science_questions = []
@@ -27,11 +37,9 @@ class Game:
         return self.how_many_players >= 2
 
     def add(self, player_name):
-        self.players.append(player_name)
-        self.places[self.how_many_players] = 0
-        self.purses[self.how_many_players] = 0
-        self.in_penalty_box[self.how_many_players] = False
-
+        newPlayer = Player(player_name)
+        self.players.append(newPlayer)
+ 
         print(player_name + " was added")
         print("They are player number %s" % len(self.players))
 
@@ -42,30 +50,32 @@ class Game:
         return len(self.players)
 
     def roll(self, roll):
-        print("%s is the current player" % self.players[self.current_player])
+        print("%s is the current player" % self.current_player)
         print("They have rolled a %s" % roll)
 
-        if self.in_penalty_box[self.current_player]: #if player is in penalty box
+        if self.players[self.current_player].in_penalty_box: #if player is in penalty box
             if roll % 2 != 0:
                 self.is_getting_out_of_penalty_box = True
 
-                print("%s is getting out of the penalty box" % self.players[self.current_player])
+                print("%s is getting out of the penalty box" % self.current_player)
                 self.continue_roll(roll)
             else:
-                print("%s is not getting out of the penalty box" % self.players[self.current_player])
+                print("%s is not getting out of the penalty box" % self.current_player)
                 self.is_getting_out_of_penalty_box = False
         else:
             self.continue_roll(roll)
 
 
     def continue_roll(self, roll):
-        self.places[self.current_player] = self.places[self.current_player] + roll
-        if self.places[self.current_player] > 11:
-            self.places[self.current_player] = self.places[self.current_player] - 12
+        current = self.players[self.current_player]
 
-        print(self.players[self.current_player] + \
+        current.places = current.places + roll
+        if  current.places > 11:
+            current.places = current.places - 12
+
+        print(current.name + \
                     '\'s new location is ' + \
-                    str(self.places[self.current_player]))
+                    str(current.places))
         print("The category is %s" % self._current_category)
         self._ask_question()
 
@@ -77,60 +87,64 @@ class Game:
 
     @property
     def _current_category(self):
-        if self.places[self.current_player] % 4 == 0: 
+        current = self.players[self.current_player]
+        if current.places % 4 == 0: 
             return 'Pop'
-        elif self.places[self.current_player] % 4 == 1:
+        elif current.places % 4 == 1:
             return 'Science'
-        elif self.places[self.current_player] % 4 == 2: 
+        elif current.places % 4 == 2: 
             return 'Sports'
         else:
             return 'Rock'
 
     def was_correctly_answered(self):
-        if self.in_penalty_box[self.current_player]:
+        if self.players[self.current_player].in_penalty_box:
             if self.is_getting_out_of_penalty_box:
-                self.answer_was_correct_(self)
+                self.answer_was_correct_()
             else:
                 self.current_player += 1
                 if self.current_player == len(self.players): self.current_player = 0
                 return True
         else:
-            self.answer_was_correct_(self)
+            self.answer_was_correct_()
 
     def answer_was_correct_(self):
-                print('Answer was correct!!!!')
-                self.purses[self.current_player] += 1
-                print(self.players[self.current_player] + \
-                    ' now has ' + \
-                    str(self.purses[self.current_player]) + \
-                    ' Gold Coins.')
+        current = self.players[self.current_player]
+        print('Answer was correct!!!!')
+        current.purses += 1
+        print(current.name + \
+            ' now has ' + \
+            str(current.purses) + \
+            ' Gold Coins.')
 
-                winner = self._did_player_win()
-                self.current_player += 1
-                if self.current_player == len(self.players): self.current_player = 0
+        winner = self._did_player_win()
+        self.current_player += 1
+        if self.current_player == len(self.players): self.current_player = 0
 
-                return winner
+        return winner
 
     def wrong_answer(self):
+        current = self.players[self.current_player]
         print('Question was incorrectly answered')
-        print(self.players[self.current_player] + " was sent to the penalty box")
-        self.in_penalty_box[self.current_player] = True
+        print(current.name + " was sent to the penalty box")
+        current.in_penalty_box = True
 
         self.current_player += 1
         if self.current_player == len(self.players): self.current_player = 0
         return True
 
     def _did_player_win(self):
-        return not (self.purses[self.current_player] == 6)
+        return not (self.players[self.current_player].purses == 6)
+
+
 
 
 from random import randrange
 
 if __name__ == '__main__':
     not_a_winner = False
-
+    
     game = Game()
-
     game.add('Chet')
     game.add('Pat')
     game.add('Sue')
@@ -149,5 +163,9 @@ if __name__ == '__main__':
 #player object (name, purse, penalty)
 #game (list of players from player class, roll,  )
 
-# class Player:
-    
+
+#work on more refactoring
+#duplication
+#if 2 lines or more, and if they appear more than once 
+
+#take game class, and slit into game and question
