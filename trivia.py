9237be15de52092c3b68105679a24da159cf2e92
bettman_latin <lbettman@lsoc.org>
2021-10-05 12:36:49 -0500

    WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- trivia.py
+++ trivia.py
@@ -4,14 +4,14 @@
 #self.players[currenty_player].whatever
 
 
-class Player:
+class Player: #creates Player Class
     def __init__(self, n):
         self.name = n
         self.places = 0
         self.purses = 0
         self.in_penalty_box = 0
 
-class Questions:
+class Questions: #creates Question Class
     def __init__(self):
         
         self.pop_questions = []
@@ -19,7 +19,7 @@ class Questions:
         self.sports_questions = []
         self.rock_questions = []
 
-        for i in range(50):
+        for i in range(50): #creates 50 of each question type and appends them to their lists
             self.pop_questions.append("Pop Question %s" % i)
             self.science_questions.append("Science Question %s" % i)
             self.sports_questions.append("Sports Question %s" % i)
@@ -35,32 +35,28 @@ class Questions:
         else:
             return 'Rock'
 
-    def _ask_question(self, current):
+    def _ask_question(self, current): #gets questions from list based on _current_category
         if self._current_category(current) == 'Pop': print(self.pop_questions.pop(0))
         if self._current_category(current) == 'Science': print(self.science_questions.pop(0))
         if self._current_category(current) == 'Sports': print(self.sports_questions.pop(0))
         if self._current_category(current) == 'Rock': print(self.rock_questions.pop(0))
 
-#link Player Class to game class
-#current-category is messed up
 
-class Game:
+
+class Game: #creates Game Class
     def __init__(self):
 
         self.players = []
         self.questions = Questions()
 
-        # self.current_questions = 0
         self.current_player = 0
         self.is_getting_out_of_penalty_box = False
 
 
-#removed create_rock question function
-
     def is_playable(self):
         return self.how_many_players >= 2
 
-    def add(self, player_name):
+    def add(self, player_name): #adds a Player object to players list
         newPlayer = Player(player_name)
         self.players.append(newPlayer)
  
@@ -69,12 +65,6 @@ class Game:
 
         return True
 
-    # def addQ(self, question):
-    #     newQuestion = Questions(question)
-    #     self.questions.append(newQuestion)
-
-
-    @property
     def how_many_players(self):
         return len(self.players)
 
@@ -95,7 +85,7 @@ class Game:
             self.continue_roll(roll)
 
 
-    def continue_roll(self, roll):
+    def continue_roll(self, roll): #method to reduce duplication
         current = self.players[self.current_player]
 
         current.places = current.places + roll
@@ -111,14 +101,14 @@ class Game:
     def was_correctly_answered(self):
         if self.players[self.current_player].in_penalty_box:
             if self.is_getting_out_of_penalty_box:
-                self.answer_was_correct_()
+                return self.answer_was_correct_()
             else:
                 self.advace_current_player()
                 return True
         else:
-            self.answer_was_correct_()
+            return self.answer_was_correct_()
 
-    def answer_was_correct_(self):
+    def answer_was_correct_(self): #method to reduce duplication
         current = self.players[self.current_player]
         print('Answer was correct!!!!')
         current.purses += 1
@@ -142,7 +132,9 @@ class Game:
         return True
 
     def _did_player_win(self):
-        return not (self.players[self.current_player].purses == 6)
+        return not (self.players[self.current_player].purses == 6) 
+        #return true if player does not have 6 coins
+        #returns false if player does have 6 coins
 
 
     def advace_current_player(self):
@@ -153,12 +145,12 @@ from random import randrange
 
 if __name__ == '__main__':
     not_a_winner = False
-    
+
     game = Game()
     game.add('Chet')
     game.add('Pat')
     game.add('Sue')
-
+    
     while True:
         game.roll(randrange(5) + 1)
 
@@ -166,8 +158,8 @@ if __name__ == '__main__':
             not_a_winner = game.wrong_answer()
         else:
             not_a_winner = game.was_correctly_answered()
-
-        if not not_a_winner: break
+            #if not_a_winner = false, then game ends
+        if not not_a_winner: break 
 
 
 #player object (name, purse, penalty)
@@ -176,6 +168,6 @@ if __name__ == '__main__':
 
 #work on more refactoring
 #duplication
-#if 2 lines or more, and if they appear more than once 
+#if 2 lines or more, and if they appear more than once
 
 #take game class, and slit into game and question
